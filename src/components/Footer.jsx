ac56223453816a0273200ea80e83ae59137904fc
petaboss2000 <petaboss2000@gmail.com>
2025-01-15 20:04:38 +0300

    site-for-dad

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<p>© 2025 Аренда манипулятора в Новосибирске</p>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>Аренда самогруза и грузоперевозки в Новосибирске</h1>
+			<div className="header-info">
+				<span>+7 (913) 705 79-30</span>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
